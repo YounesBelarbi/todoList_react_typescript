a9cd94c051699b8382d314ac9469a13cff22c9f0
younesbelarbi <ybelarbi@outlook.com>
2024-01-27 20:58:33 +0100

    Revert "build: install nanoid"
    This reverts commit 21aba6a605a010ec5b91ce121b7581ec4837c17e.

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,15 +1,21 @@
 import { createContext, useState } from "react"
 
 interface TodoContextProps {
-    todos: string[]
+    todos: Todo[]
     addTodo: (todo: string) => void
+}
+
+interface Todo {
+    id: number
+    text: string
+    status: 'pending' | 'completed'
 }
 
 export const TodoContext = createContext<TodoContextProps | undefined>(undefined)
 export const TodoProvider = (props: { children: React.ReactNode } ) => {
-    const [todos, setTodos] = useState<string[]>([])
+    const [todos, setTodos] = useState<Todo[]>([])
 
-    const addTodo = (todo: string) => {
+    const addTodo = (todo: Todo) => {
         setTodos([...todos, todo])
     }
 
